168544983fb376fa59dfc1213f65fc76885a357c
Krishna Teja Sunkara <ktsunkara@gmail.com>
2023-05-16 11:02:27 +0530

    initial commit

---
+++ coveys-quad-backend/src/main/java/com/example/coveysquadbackend/CoveysQuadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coveysquadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoveysQuadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoveysQuadBackendApplication.class, args);
+	}
+
+}
---
+++ coveys-quad-backend/src/test/java/com/example/coveysquadbackend/CoveysQuadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coveysquadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoveysQuadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
